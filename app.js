18c00fc37e5bb673fced3d5f6cdcf78b1ded90aa
Amir Ali <amiralirang98@gmail.com>
2020-11-02 23:33:28 +0530

    init

---
+++ app.js
@@ -0,0 +1,16 @@
+var express= require('express')
+var app= express()
+app.get('/',function(req,res){
+    res.render('index.ejs')
+})
+app.listen('3000',function(err){
+    if(err)
+    {
+        console.log(err)
+    }
+    else
+    {
+        console.log('server started successfully')
+    }
+
+}) 
